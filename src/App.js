310c7d55c58f89960b312dd18ff0a37549cba410
Adam Hopkins <Samsung3382@outlook.com>
2022-11-12 18:03:01 -0500

    Updated Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+import React from 'react';
+import Nav from './components/Nav';
+import Blog from './components/Blog';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Blog/>
+    </div>
+  );
+}
+
+export default App;
