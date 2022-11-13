63c7a86001fbdebce653518e1cf69fb0a1329f72
Adam Hopkins <Samsung3382@outlook.com>
2022-11-12 23:51:50 -0500

    created footer and implemented SCSS styling.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './index.css';
+import './SCSS/footer.css';
 import React from 'react';
 import Nav from './components/Nav';
 import Blog from './components/Blog';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Blog/>
+      <Footer/>
     </div>
   );
 }
